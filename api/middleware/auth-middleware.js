9bf2d850fd5f09dfb49d40a4a26e5a9662c1486b
Bobby <monkeyeden.rjt@gmail.com>
2021-08-20 11:31:39 -0400

    wip

--- api/middleware/auth-middleware.js
+++ api/middleware/auth-middleware.js
@@ -1,5 +1,31 @@
 const User = require('./../users/users-model')
 
+const validatePayload = async (req,res,next)=>{
+    try{
+        const {username,password} = req.body
+        if(!username|| !password){
+            next({status:401,message:'username and password required'})
+        }else{
+            next()
+        }
+    }catch(err){
+        next(err)
+    }
+}
+
+const usernameTaken = async (req,res,next) =>{
+    try{
+        const [user] = await User.getBy({username:req.body.username}) 
+        if(user){
+            next({status:401,message:'username taken'})
+        }else{
+            next()
+        }
+    }catch(err){
+        next(err)
+    }
+}
+
 const usernameExists = async (req, res, next) => {
    try{
     const [user] = await User.getBy({username:req.body.username})
@@ -15,6 +41,9 @@ const usernameExists = async (req, res, next) => {
   }
 
 module.exports = {
-    usernameExists
+    usernameExists,
+    validatePayload,
+    usernameTaken
+    
 }
 
